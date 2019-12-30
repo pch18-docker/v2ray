deed36a2ebf5deb56e3ca0c1dbbc19e199c23360
pch18 <pch18@qq.com>
2019-12-30 19:12:14 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,11 +9,7 @@ uuid_list="${uuid_list:1}"
 
 cp /etc/v2ray/config.json.tmp /etc/v2ray/config.json
 sed -i "s/\$UUID_LIST/${uuid_list}/" /etc/v2ray/config.json
-
-cp /etc/caddy/caddy.conf.tmp /etc/caddy/caddy.conf
-sed -i "s/\$DOMAIN/${DOMAIN}/" /etc/caddy/caddy.conf
-sed -i "s/\$MAIL/${MAIL}/" /etc/caddy/caddy.conf
-sed -i "s/\$REDIRECT_URL/${REDIRECT_URL//\//\\\/}/" /etc/caddy/caddy.conf
+sed -i "s/\$RAY_PATH/${RAY_PATH}/" /etc/v2ray/config.json
 
 caddy -agree -conf /etc/caddy/caddy.conf >&1 &
 v2ray -config=/etc/v2ray/config.json 
