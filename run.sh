83773acd4df5092454f6e9d5f253120511d80f40
pch18 <pch18@qq.com>
2021-03-14 20:54:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ cp /etc/v2ray/config.json.tmp /etc/v2ray/config.json
 sed -i "s/\$UUID_LIST/${uuid_list}/" /etc/v2ray/config.json
 sed -i "s/\$RAY_PATH/${RAY_PATH}/" /etc/v2ray/config.json
 
-caddy -agree -conf /etc/caddy/caddy.conf -email ${EMAIL} >&1 &
+caddy start --config /etc/caddy/caddy.conf --adapter caddyfile >&1 &
 v2ray -config=/etc/v2ray/config.json
