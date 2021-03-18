047c0ddb0adead8c497bbf7925b837aa2715f6b3
潘晨昊 <panchenhao@bytedance.com>
2021-03-18 19:26:02 +0800

    fix

--- start.sh
+++ start.sh
@@ -3,13 +3,11 @@ URL_V2RAY_CONFIG=https://raw.githubusercontent.com/pch18-docker/v2ray/master/v2r
 URL_MONITOR_CONFIG=https://raw.githubusercontent.com/pch18-docker/v2ray/master/monitor.sh
 
 
-
-
 # 拉取caddy配置, 执行
 wget ${URL_CADDY_CONFIG} -O /etc/caddy/caddy.conf
 caddy run --config /etc/caddy/caddy.conf --adapter caddyfile >&1 &
 
-# 拉取v2ray配置模板, 写入uuid信息
+# 拉取v2ray配置模板, 写入uuid信息, 执行
 wget ${URL_V2RAY_CONFIG} -O /etc/v2ray/v2ray.json
 uuid_list=''
 for line in `env|grep '^UUID_.*'`
@@ -25,7 +23,7 @@ sed -i "s/\$PATH_RAY/\\${PATH_RAY}/" /etc/v2ray/v2ray.json
 # 打开流量监控
 wget -O /monitor.sh ${URL_MONITOR_CONFIG}
 chmod 777 /monitor.sh
-sh -c "watch -n 60 /monitor.sh">&1 &
+# sh -c "watch -n 60 /monitor.sh">&1 &
 
 # 主进程
 v2ray --config=/etc/v2ray/v2ray.json
